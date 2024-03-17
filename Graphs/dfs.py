c48922fa60fe8aaf048a3d849fa55313f71653bc
João Assalim <joassalim@gmail.com>
2024-03-17 11:24:04 -0300

    Update dfs.py

--- Graphs/dfs.py
+++ Graphs/dfs.py
@@ -15,20 +15,36 @@ Time Complexity:
 
 """
 
-def dfs(x, marked=[]):
-    marked.append(x)
+def dfs_iter(x):
+    stack = [x]
+    visited = []
+    
+    while stack:
+        v = stack.pop()
+        if not v in visited:
+            visited.append(v)
+            for i in graph[v]:
+                stack.append(i)
+    
+    print(visited)
+
+
+def dfs_rec(x, visited=[]):
     print(x, end=" ")
+    visited.append(x)
+    
+    for v in graph[x]:
+        if not v in visited:
+            dfs_rec(v, visited)
+    
 
-    for w in graph[x]:
-        if not w in marked:
-            dfs(w, marked)
-
-n, m = list(map(int, input().split())) # nodes, edges
+n, m = list(map(int, input().split()))
 graph = [[] for i in range(n)]
 
 for i in range(m):
     a, b = list(map(int, input().split()))
     graph[a].append(b)
-    graph[b].append(a)
 
-dfs(0)
+dfs_iter(0)
+print()
+dfs_rec(0)
